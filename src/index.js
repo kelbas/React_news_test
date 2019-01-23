7b29b5064ae3a393d7c52fdb9ff149908c87d962
kelbas <kelbasHD@gmail.com>
2019-01-23 19:54:34 +0200

    id='test'

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import {render} from 'react-dom'
 import App from './components/App'
 
-render(<App></App>, document.getElementById('body'))
+render(<App></App>, document.getElementById('test'))
